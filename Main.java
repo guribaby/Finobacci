856a89d522727750a5ea3a2aecd8ae1d11344307
guribaby <122307456+guribaby@users.noreply.github.com>
2023-05-04 19:33:15 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package actividad;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+       Manager FINOBACCI = new Manager();
+        FINOBACCI.correrPrograma();
+
+
+    }
+}
